7a86bf9083de046a0d8e5e6393ea5fbbd6e69f30
Dengfeng Liu <liudf0716@gmail.com>
2021-03-17 19:21:01 +0800

    Update fetch_image.sh

--- fetch_image.sh
+++ fetch_image.sh
@@ -12,9 +12,9 @@ else
 fi
 
 # install etcd
-sudo docker run -d --restart --network host --name etcd -p 2379:2379 -e ETCDCTL_API=3 \
+sudo docker run -d  --network host --name etcd -p 2379:2379 -e ETCDCTL_API=3 \
         quay.io/coreos/etcd /usr/local/bin/etcd \
         -advertise-client-urls http://0.0.0.0:2379 -listen-client-urls http://0.0.0.0:2379
 
 # install vpp-wrt
-sudo docker run -d --restart --network host --name vpp-wrt -p 5002:5002 -p 9191:9191 --privileged liudf0716/vpp-wrt:20.09
+sudo docker run -d  --network host --name vpp-wrt -p 5002:5002 -p 9191:9191 --privileged liudf0716/vpp-wrt:20.09
